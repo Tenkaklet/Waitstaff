0e9de55640897a0131185018ad53e5c1e8415bfc
Tenkaklet <tenkaklet@gmail.com>
2015-11-08 15:56:17 +0100

    begin work on functionality

--- js/master.js
+++ js/master.js
@@ -1,7 +1,13 @@
 angular.module('waitstaffApp', []).controller('waitCtrl', [function (){
     var self = this;
 
+
+
+    var mealPrice = self.mealPrice;
     self.submit = function () {
+        var tip = self.input.tip;
+        var base = self.input.mealPrice;
         console.log('Input prices of:', self.input);
+        console.log(tip + base);
     };
 }]);
