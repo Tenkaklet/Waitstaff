a17d2ae4b7c26dda4a2a0ce86e096b812d41a64f
Tenkaklet <tenkaklet@gmail.com>
2015-11-08 15:24:44 +0100

    introduced angular

--- js/master.js
+++ js/master.js
@@ -1,1 +1,7 @@
+angular.module('waitstaffApp', []).controller('waitCtrl', [function (){
+    var self = this;
 
+    self.submit = function () {
+        console.log('Input prices of:', self.input);
+    };
+}]);
