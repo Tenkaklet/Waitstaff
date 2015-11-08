ab52a0660e9755c7a307ed56696ecb947f0f5211
Tenkaklet <tenkaklet@gmail.com>
2015-11-08 19:24:06 +0100

    began subtotal work

--- js/master.js
+++ js/master.js
@@ -1,14 +1,16 @@
 angular.module('waitstaffApp', []).controller('waitCtrl', [function (){
     var self = this;
 
-
-
-    var mealPrice = self.mealPrice;
     self.submit = function () {
         var tip = self.input.tip;
         var base = self.input.mealPrice;
+        var tax = self.input.tax;
         console.log('Input prices of:', self.input);
         //Calculate Tip
-        console.log((tip + base)/100);
+        // console.log((tip + base)/100);
+        console.log(base + (base * (tax/100)));
+        self.subTotal = base + (base * (tax/100));
+        console.log(self.subTotal);
+
     };
 }]);
